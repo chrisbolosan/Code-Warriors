e0e4cf8af54b8ab09e024a7048306128b203db01
Galower <ariosgalower@gmail.com>
2021-08-23 15:45:32 -0400

    fixed for deployment

--- src/Routes.js
+++ src/Routes.js
@@ -1,31 +1,31 @@
-import React, { Component } from "react"
-import { connect } from "react-redux"
-import { withRouter, Route, Switch, Redirect } from "react-router-dom"
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { withRouter, Route, Switch, Redirect } from 'react-router-dom';
 // import Home from "./components/Home"
 //import Leaderboard from "./components/Leaderboard"
 //import UserProfile from "./components/UserProfile"
-import GuestHome from "./components/GuestHome"
-import Login from "./components/Login"
-import SignUp from "./components/Signup"
-import IDE from "./components/IDE";
+import GuestHome from './components/GuestHome';
+import Login from './components/Login';
+import SignUp from './components/SignUp';
+import IDE from './components/IDE';
 // import { Login, SignUp } from "./components/AuthForm"
-
 
 class Routes extends Component {
   componentDidMount() {
     // do something here
   }
   render() {
-    return(
-        <Switch>
-          {// these routes are for non-logged in users, except for the ide
-          }
-          <Route exact path="/" component ={GuestHome} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/signup" component={SignUp} />
-          <Route exact path="/IDE" component={IDE} />
-        </Switch>
-    )
+    return (
+      <Switch>
+        {
+          // these routes are for non-logged in users, except for the ide
+        }
+        <Route exact path="/" component={GuestHome} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/signup" component={SignUp} />
+        <Route exact path="/IDE" component={IDE} />
+      </Switch>
+    );
   }
 }
 
@@ -66,4 +66,4 @@ const mapDispatch = (dispatch) => {
 export default withRouter(connect(mapState, mapDispatch)(Routes))
 
 */
-export default withRouter(connect()(Routes))
+export default withRouter(connect()(Routes));
