bde54abf6899ed148cb6e02237e8cd8d8c849125
Haram Chang <harammchang@gmail.com>
2021-08-25 14:44:06 -0400

    cleaned up code

--- src/components/IDEOpponent.js
+++ src/components/IDEOpponent.js
@@ -4,29 +4,6 @@ import "codemirror/theme/material.css";
 import "codemirror/addon/edit/closebrackets";
 import "codemirror/mode/javascript/javascript";
 import { Controlled } from "react-codemirror2";
-//import { ToastContainer } from "react-toastify";
-//import "react-toastify/dist/ReactToastify.css";
-/*
-import socket from "socket.io-client"
-
-// window.location.origin describes the URL of the page we're on
-const clientSocket = socket(window.location.origin)
-
-// Run when connected to server
-clientSocket.on("connect", () => {
-  console.log("Connected to server")
-})
-
-clientSocket.on("message", message => {
-  console.log(message)
-})
-
-clientSocket.on("solution", solutionText => {
-  const newDiv = document.createElement("div")
-  newDiv.innerText = solutionText
-  const testing = document.getElementById("testing")
-  testing.appendChild(newDiv)
-})*/
 
 class IDEOpponent extends React.Component {
   constructor(props) {
@@ -34,7 +11,6 @@ class IDEOpponent extends React.Component {
   }
 
   render() {
-
     const options = {
       lineWrapping: true,
       lint: true,
