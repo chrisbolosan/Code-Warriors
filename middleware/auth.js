b21f6f4744b8a247cd23fb671f2f442f92f063d8
Galower <ariosgalower@gmail.com>
2021-08-26 00:57:41 -0400

    fully functional backend

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,25 +1,25 @@
-const jwt = require("jsonwebtoken");
-const asyncHandler = require("./async");
-const ErrorResponse = require("../utils/errorResponse");
-const User = require("../models/User");
+const User = require('../models/User');
 
 // gatekeeping middleware protect routes
 //adds requests to the req variables along with try catch
-exports.protect = asyncHandler(async (req, res, next) => {
+const authHandler = async (req, res, next) => {
   try {
     const { isAdmin, username, _id, rank } = await User.findByToken(
       req.headers.authorization
     );
 
     if (!username) {
-      return next(new ErrorResponse("No token found", 401));
+      res.status(401).send('Invalid token, no user found');
     }
 
-    req.rank = rank;
     req.admin = isAdmin;
-    req._id = _id;
+    req.id = _id;
+    req.rank = rank;
+    next();
   } catch (err) {
-    console.error(err);
-    return next(new ErrorResponse("Not Authorized to access this route", 401));
+    res.status(401).send('Invalid token or no token found');
+    next(err);
   }
-});
+};
+
+module.exports = authHandler;
--- middleware/test.js
+++
@@ -1,8 +0,0 @@
-const jwt = require("jsonwebtoken");
-
-const { id } = jwt.verify(
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxMjNjYWEzYTBiODRjYWYyMTdmM2RjNiIsImlhdCI6MTYyOTk0MzM3NSwiZXhwIjoxNjMyNTM1Mzc1fQ.pCau7zTqiXMUJ90hbPrihEnZBt4oQ6Apu5_BnUzd9RA",
-  "banana"
-);
-
-console.log(id);
