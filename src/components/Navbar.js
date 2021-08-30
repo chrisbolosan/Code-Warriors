b6f7a7b12ef1458fd8eb9bf96d69898904d50a30
Galower <ariosgalower@gmail.com>
2021-08-30 02:56:04 -0400

    more styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -25,7 +25,7 @@ const Navbar = ({ isLoggedIn, handleClick, userId, auth }) => {
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
-          <span className="navbar-toggler-icon"></span>
+          <span className="dropdown">+</span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
