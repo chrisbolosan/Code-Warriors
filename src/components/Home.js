319abf6dcaea486ff11c93bc7fda6cc8a0a4491b
Haram Chang <harammchang@gmail.com>
2021-09-05 01:03:04 -0400

    added a modal

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,10 @@ import React, { useState, useEffect } from 'react';
 import clientSocket from '../socket/socket';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { makeStyles } from '@material-ui/core/styles';
+import { Modal } from "@material-ui/core"
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
 import { setRooms, addRoom, updateRoom } from '../store/rooms';
 import { getRandomExercise, getFilteredExercise } from '../store/exercise';
 import DifficultyFilter from './DifficultyFilter';
@@ -14,10 +18,11 @@ const Home = (props) => {
     updateRoom,
     getFilteredExercise,
   } = props;
+
   const [rooms, setRooms] = useState([]);
   const [roomId, setRoomId] = useState(0);
+  const [open, setOpen] = React.useState(false)
   const [gameTime, setGameTime] = useState(300000);
-  const [fExercise, setExercise] = useState({});
   const [difficulty, setDifficulty] = useState('');
 
   useEffect(() => {
@@ -32,6 +37,34 @@ const Home = (props) => {
     setRoomId(randomRoomId);
     getRandomExercise();
   }, []);
+
+  const useStyles = makeStyles((theme) => ({
+  modal: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: "black"
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+    height: "300px",
+    width: "400px;"
+  },
+  }));
+
+  const classes = useStyles()
+
+
+  function handleCreate() {
+    setOpen(true)
+  }
+
+  function handleClose() {
+    setOpen(false)
+  }
 
   // when a user clicks creates a game
   async function handleClick() {
@@ -53,7 +86,7 @@ const Home = (props) => {
     clientSocket.emit('createGame', roomId);
   }
 
-  //Update the time for the game
+  // update the time for the game
   function updateTime(event) {
     const time = event.target.value;
     setGameTime(time);
@@ -111,79 +144,109 @@ const Home = (props) => {
   }
 
   return (
-    <div className="main-home">
-      <h1 className="welcomebanner">Welcome! Create or Join a Game</h1>
-      <div id="gameFilters">
-        <DifficultyFilter
-          handleChange={handleChange}
-          difficulty={difficulty}
-          setDifficulty={setDifficulty}
-        />
-        {/* Time Selector */}
-        <div>
-          <label id="time-container">
-            {' '}
-            <select
-              onChange={updateTime}
-              name="times"
-              id="timesSelector"
-              class="form-select"
-            >
-              <option value="300000">5 Minutes</option>
-              <option value="600000">10 Minutes</option>
-              <option value="900000">15 Minutes</option>
-              <option value="1200000">20 Minutes</option>
-              <option value="1500000">25 Minutes</option>
-              <option value="1800000">30 Minutes</option>
-            </select>
-          </label>
+    <div id="home-container" className="flex">
+
+      <div id="home-battles">
+
+        <div id="gameFilters">
+          <DifficultyFilter
+            handleChange={handleChange}
+            difficulty={difficulty}
+            setDifficulty={setDifficulty}
+          />
+          {/* Time Selector */}
+          <div>
+            <label id="time-container">
+              {' '}
+              <select
+                onChange={updateTime}
+                name="times"
+                id="timesSelector"
+                class="form-select"
+              >
+                <option value="300000">5 Minutes</option>
+                <option value="600000">10 Minutes</option>
+                <option value="900000">15 Minutes</option>
+                <option value="1200000">20 Minutes</option>
+                <option value="1500000">25 Minutes</option>
+                <option value="1800000">30 Minutes</option>
+              </select>
+            </label>
+          </div>
+        </div>
+
+        {/* Create Game */}
+        {/* <Link
+          to={{
+            pathname: '/game',
+            state: {
+              roomId: roomId,
+              exerciseId: props.exercise._id,
+              gameTime: gameTime,
+            },
+          }}
+        > */}
+          <button type="button" id="create-button" onClick={handleCreate}>
+            Create Game
+          </button>
+          <Modal
+            className={classes.modal}
+            open={open}
+            onClose={handleClose}
+            closeAfterTransition
+            BackdropComponent={Backdrop}
+            BackdropProps={{
+              timeout: 500,
+            }}
+          >
+            <Fade in={open}>
+              <div className={classes.paper}>
+                <p>Pick ur difficulty level</p>
+              </div>
+            </Fade>
+          </Modal>
+        {/* </Link> */}
+        <h2>Rooms</h2>
+        <div className="rooms-container">
+          {props.battles[0] &&
+            props.battles.map((room) => {
+              return (
+                // JOIN GAME Button
+                <div className="room-item">
+                  <Link
+                    to={{
+                      pathname: '/game',
+                      state: {
+                        roomId: room.roomId,
+                        exerciseId: room.ref,
+                      },
+                    }}
+                  >
+                    <button
+                      onClick={joinRoom}
+                      value={room.roomId}
+                      name={room._id}
+                      key={room._id}
+                      type="button"
+                      className="room-button"
+                    >{`${
+                      room.players ? room.players[0].username : 'User'
+                    }'s room`}</button>
+                  </Link>
+                </div>
+              );
+            })}
         </div>
       </div>
-      {/* Create Game */}
-      <Link
-        to={{
-          pathname: '/game',
-          state: {
-            roomId: roomId,
-            exerciseId: props.exercise._id,
-            gameTime: gameTime,
-          },
-        }}
-      >
-        <button type="button" id="create-button" onClick={handleClick}>
-          Create Game
-        </button>
-      </Link>
-      <h2>Rooms</h2>
-      <div className="rooms-container">
-        {props.battles[0] &&
-          props.battles.map((room) => {
-            return (
-              // JOIN GAME Button
-              <div className="room-item">
-                <Link
-                  to={{
-                    pathname: '/game',
-                    state: {
-                      roomId: room.roomId,
-                      exerciseId: room.ref,
-                    },
-                  }}
-                >
-                  <button
-                    onClick={joinRoom}
-                    value={room.roomId}
-                    name={room._id}
-                    key={room._id}
-                    type="button"
-                    className="room-button"
-                  >{`${
-                    room.players ? room.players[0].username : 'User'
-                  }'s room`}</button>
-                </Link>
-              </div>
-            );
-          })}
+
+      <div id="home-user" className="flex">
+        <h3 id="welcome">{`Hi, ${props.auth.username}`}</h3>
+        <div id="dashboard">
+          <p>rank: {props.auth.rank}</p>
+          <p>points: {props.auth.totalPoints}</p>
+          <p>matches won:</p>
+          <p>matches played:</p>
+        </div>
       </div>
     </div>
   );
